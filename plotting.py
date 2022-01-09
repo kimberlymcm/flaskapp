20922a6e1864457796af3325c9bbcfae50143728
Kimberly McManus <kimberlymcmanus@kimberlys-air.lan>
2022-01-09 13:21:18 -0800

    Update dashboard UI

--- plotting.py
+++ plotting.py
@@ -12,7 +12,7 @@ def create_barplot1(df):
     fig.add_trace(go.Scatter(x=df["time"], y=df["pm1"], name="pm1"))
     fig.add_trace(go.Scatter(x=df["time"], y=df["pm10"], name="pm10"))
     fig.add_trace(go.Scatter(x=df["time"], y=df["pm25"], name="pm25"))
-    fig.update_layout(title="Particulate Matter", xaxis_title="Time", yaxis_title="ug/m^3",
+    fig.update_layout(xaxis_title="Time", yaxis_title="ug/m^3",
         legend_title="Particulate Size", font=dict(family="Open Sans", size=10),
         margin=dict(l=35, r=15, b=30, t=30))
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
@@ -23,7 +23,7 @@ def create_barplot2(df):
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=df["time"], y=df["temperature"], name="temperature (C)"))
     fig.add_trace(go.Scatter(x=df["time"], y=df["humidity"], name="humidity (%)"))
-    fig.update_layout(title="Temperature and Humidity", xaxis_title="Time", yaxis_title="",
+    fig.update_layout(xaxis_title="Time", yaxis_title="",
         font=dict(family="Open Sans", size=10),
         margin=dict(l=35, r=15, b=30, t=30))
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
@@ -35,7 +35,7 @@ def create_barplot3(df):
     fig.add_trace(go.Scatter(x=df["time"], y=df["oxidised"], name="oxidised"))
     fig.add_trace(go.Scatter(x=df["time"], y=df["reduced"], name="reduced"))
     fig.add_trace(go.Scatter(x=df["time"], y=df["nh3"], name="nh3"))
-    fig.update_layout(title="Gases (unnormalized units)", xaxis_title="Time", yaxis_title="",
+    fig.update_layout(xaxis_title="Time", yaxis_title="",
         legend_title="Gases",
         font=dict(family="Open Sans", size=10),
         margin=dict(l=35, r=15, b=30, t=30))
@@ -49,7 +49,7 @@ def create_barplot4(df):
     fig.add_trace(go.Scatter(x=df["time"], y=df["lux"], name="lux"))
     fig.add_trace(go.Scatter(x=df["time"], y=df["pressure"], name="pressure"),
         secondary_y=True)
-    fig.update_layout(title="Light and pressure", xaxis_title="Time", legend_title="",
+    fig.update_layout(xaxis_title="Time", legend_title="",
         font=dict(family="Open Sans", size=10),
         margin=dict(l=35, r=15, b=30, t=30))
     fig.update_yaxes(title_text="Light Y-axis", secondary_y=False)
