f395f026b5d6db7ab00a441d6b55c13dbe41233c
Kimberly McManus <kimberlymcmanus@kimberlys-air.lan>
2022-01-09 13:36:02 -0800

    Fixed merge conflicts

--- plotting.py
+++ plotting.py
@@ -13,8 +13,15 @@ def create_barplot1(df):
     fig.add_trace(go.Scatter(x=df["time"], y=df["pm10"], name="pm10"))
     fig.add_trace(go.Scatter(x=df["time"], y=df["pm25"], name="pm25"))
     fig.update_layout(xaxis_title="Time", yaxis_title="ug/m^3",
-        legend_title="Particulate Size", font=dict(family="Open Sans", size=10),
-        margin=dict(l=35, r=15, b=30, t=30))
+        font=dict(family="Open Sans", size=10),
+        margin=dict(l=25, r=10, b=10, t=10),
+        legend=dict(
+            orientation="h",
+            yanchor="top",
+            y=0.99,
+            xanchor="left",
+            x=0.01)
+        )
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
     return graphJSON 
 
@@ -25,7 +32,14 @@ def create_barplot2(df):
     fig.add_trace(go.Scatter(x=df["time"], y=df["humidity"], name="humidity (%)"))
     fig.update_layout(xaxis_title="Time", yaxis_title="",
         font=dict(family="Open Sans", size=10),
-        margin=dict(l=35, r=15, b=30, t=30))
+        margin=dict(l=25, r=10, b=10, t=10),
+        legend=dict(
+            orientation="h",
+            yanchor="top",
+            y=0.99,
+            xanchor="left",
+            x=0.01)
+        )
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
     return graphJSON
 
@@ -36,9 +50,15 @@ def create_barplot3(df):
     fig.add_trace(go.Scatter(x=df["time"], y=df["reduced"], name="reduced"))
     fig.add_trace(go.Scatter(x=df["time"], y=df["nh3"], name="nh3"))
     fig.update_layout(xaxis_title="Time", yaxis_title="",
-        legend_title="Gases",
         font=dict(family="Open Sans", size=10),
-        margin=dict(l=35, r=15, b=30, t=30))
+        margin=dict(l=25, r=10, b=10, t=10),
+        legend=dict(
+            orientation="h",
+            yanchor="top",
+            y=0.99,
+            xanchor="left",
+            x=0.01)
+        )
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
     return graphJSON
 
@@ -49,9 +69,16 @@ def create_barplot4(df):
     fig.add_trace(go.Scatter(x=df["time"], y=df["lux"], name="lux"))
     fig.add_trace(go.Scatter(x=df["time"], y=df["pressure"], name="pressure"),
         secondary_y=True)
-    fig.update_layout(xaxis_title="Time", legend_title="",
+    fig.update_layout(xaxis_title="Time",
         font=dict(family="Open Sans", size=10),
-        margin=dict(l=35, r=15, b=30, t=30))
+        margin=dict(l=25, r=10, b=10, t=10),
+        legend=dict(
+            orientation="h",
+            yanchor="top",
+            y=0.99,
+            xanchor="left",
+            x=0.01)
+        )
     fig.update_yaxes(title_text="Light Y-axis", secondary_y=False)
     fig.update_yaxes(title_text="Pressure Y-axis", secondary_y=True)
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
